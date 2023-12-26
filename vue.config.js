14c97b7f04b56d6c92ceb27787b74f89f59f9563
Jackson Wang <jianingwang124@gmai.com>
2023-12-26 17:25:06 +0800

    added workflow

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
     proxy: {
       //名字可以自定义，这里我用的是api
       '/api': {
-        target: "http://localhost:8809", 
+        target: "http://localhost:8809",
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
@@ -18,5 +18,5 @@ module.exports = defineConfig({
       }
     }
   },
-  outputDir: "./dist",
+  outputDir: "./build/dist",
 })
