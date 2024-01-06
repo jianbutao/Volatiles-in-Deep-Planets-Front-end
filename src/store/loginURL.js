7648a4db797cf87757fa4c000829f1a5af0ce721
IEeyaa <3200103483@zju.edu.cn>
2024-01-07 01:02:05 +0800

    20240107 syt bug修复

--- src/store/loginURL.js
+++ src/store/loginURL.js
@@ -1,11 +1,13 @@
 export const loginURL = {
+    baseURL: 'https://test-user-oneid.deep-time.org',
+    
     appCode: 'htg',
 
     domain: 'deep-time.org',
 
     secretCode: '9abxmzzbf8kvalvlsqoybd77fcnl54gu',
 
-    login: '/login/ngiam-rst/v1/sdk/login/sso',
+    login: '/ngiam-rst/v1/sdk/login/sso',
 
     tokenChange: '/login/ngiam-rst/oauth2/token',
 
